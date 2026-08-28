0e540a17b42451ddedd10dd1d255500f3270bc4a
archie5915 <69157397+archie5915@users.noreply.github.com>
2026-08-28 22:01:09 +0800

    fix: removed web_proxy variable

--- gcp-no-encryption/gcp-output.tf
+++ gcp-no-encryption/gcp-output.tf
@@ -11,7 +11,6 @@ ssh ubuntu@${google_compute_address.ph-public-ip.address}
 
 ## WebUI (VPN) ##
 https://${google_compute_address.ph-public-ip.address}/admin/
-(https://${var.docker_webproxy}/admin/)
 
 ## Wireguard Configurations ##
 https://console.cloud.google.com/storage/browser/${var.ph_prefix}-bucket-${random_string.ph-random.result}/wireguard?project=${var.ph_prefix}-project-${random_string.ph-random.result}
@@ -24,7 +23,7 @@ https://console.cloud.google.com/storage/browser/${var.ph_prefix}-bucket-${rando
 ssh ubuntu@${google_compute_address.ph-public-ip.address}
 
 # Remove containers (services are down until ansible step completes!)
-sudo docker rm -f cloudflared_doh pihole web_proxy wireguard
+sudo docker rm -f cloudflared_doh pihole wireguard
 
 # Re-apply Ansible playbook via systemctl
 sudo systemctl start cloudblock-ansible-state.service
