a4a07be4a85237b70eab240f507463d10f53339c
Archie Hernandez <archie5915@gmail.com>
2026-08-29 19:04:58 +0800

    Use uniform bucket-level access

--- gcp-no-encryption/gcp-storage.tf
+++ gcp-no-encryption/gcp-storage.tf
@@ -1,24 +1,22 @@
-data "google_storage_project_service_account" "ph-storage-account" {
-  project = google_project.ph-project.project_id
-}
-
 resource "google_storage_bucket" "ph-bucket" {
-  name     = "${var.ph_prefix}-bucket-${random_string.ph-random.result}"
-  location = var.gcp_region
-  project  = google_project.ph-project.project_id
+  name                        = "${var.ph_prefix}-bucket-${random_string.ph-random.result}"
+  location                    = var.gcp_region
+  project                     = google_project.ph-project.project_id
+  uniform_bucket_level_access = true
   versioning {
     enabled = false
   }
   force_destroy = true
 }
 
-resource "google_storage_bucket_acl" "ph-bucket-acl" {
+resource "google_storage_bucket_iam_member" "ph-bucket-user-admin" {
   bucket = google_storage_bucket.ph-bucket.name
-  role_entity = [
-    "OWNER:project-owners-${google_project.ph-project.number}",
-    "OWNER:project-editors-${google_project.ph-project.number}",
-    "READER:project-viewers-${google_project.ph-project.number}",
-    "OWNER:user-${var.gcp_user}",
-    "OWNER:user-${google_service_account.ph-service-account.email}"
-  ]
+  role   = "roles/storage.admin"
+  member = "user:${var.gcp_user}"
+}
+
+resource "google_storage_bucket_iam_member" "ph-bucket-service-account-object-admin" {
+  bucket = google_storage_bucket.ph-bucket.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.ph-service-account.email}"
 }
